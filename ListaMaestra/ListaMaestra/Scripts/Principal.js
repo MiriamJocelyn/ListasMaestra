6b76943b367c6d31d667fb1a560c6cb566e9efa8
Github2715 <mirismarquez@gmail.com>
2022-12-06 22:59:38 -0600

    Busqueda

--- ListaMaestra/ListaMaestra/Scripts/Principal.js
+++ ListaMaestra/ListaMaestra/Scripts/Principal.js
@@ -1,31 +1,130 @@
 ﻿$(document).ready(function () {
 
-    prueba()
+    //prueba()
+
+    jQuery.ajax({
+        async: true,
+        url: "http://localhost:57199/ListaM/ConsultaNivel",
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: null,
+        success: function (data) {
+            llenadoComboN(data)
+            if (data.length > 0)
+                $("#lblNivel").text(data[0].nivel)
+        },
+        error: function (data) {
+            console.log(data)
+            alert(data)
+        }
+    });
+
+    jQuery.ajax({
+        async: true,
+        url: "http://localhost:57199/ListaM/ConsultaArea",
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: null,
+        success: function (data) {
+            llenadoComboA(data)
+            if (data.length > 0)
+                $("#lblArea").text(data[0].area)
+        },
+        error: function (data) {
+            console.log(data)
+            alert(data)
+        }
+    });
+
+    $("#cmbNivel").change(function () {
+        $("#lblNivel").text($(this).val())
+    })
+
+    $("#cmbArea").change(function () {
+        $("#lblArea").text($(this).val())
+    })
+
     $("#btnBuscar").click(function () {
         var habilitado = $("#cbmHabilitado option:selected").val();
-        var area = $("#cbmArea option:selected").val();
-        var nivel = $("#cbmNivel option:selected").val();
+        var area = $("#lblArea").text()
+        var nivel = $("#lblNivel").text()
 
-        
+        var object = { estatus: habilitado, area: area, nivel: nivel }
+        console.log(object)
+        jQuery.ajax({
+            async: true,
+            url: "http://localhost:57199/ListaM/ConsultaLista",
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            data: JSON.stringify(object),
+            success: function (data) {
+                console.log('lista')
+                console.log(data)
+                llenadoGrid(data)
+            },
+            error: function (data) {
+                console.log(data)
+                alert(data)
+            }
+        });
+
+
     })
     //alert($("#2LP01").attr('checked'))
 })
 
-function check()
-{
-    if ($(this).attr('checked')) {
-        $(this).removeAttr('checked')
-        var a = $(this).attr('checked')
-        alert(a)
-    } else {
-        $(this).attr('checked', 'checked')
-        var a = $(this).attr('checked')
-        alert(a)
-    }
+function llenadoComboN(data) {
+    for (i = 0; i < data.length; i++)
+        $("#cmbNivel").append($("<option>", { value: data[i].nivel, text: data[i].nivel }))
 }
-function busqueda(){
 
+function llenadoComboA(data) {
+    for (i = 0; i < data.length; i++)
+        $("#cmbArea").append($("<option>", { value: data[i].area, text: data[i].area }))
 }
+
+function llenadoGrid(data) {
+    var htmlTbody=""
+    for (i = 0; i < data.length; i++) {
+        htmlTbody += '<tr>'
+        htmlTbody += '<td><input type="checkbox" id="' + data[i].nivel + data[i].area + data[i].Doc + '" onclick="check()"/></td>'
+        htmlTbody += '<td>' + data[i].nivel + '</td>'
+        htmlTbody += '<td>' + data[i].area + '</td>'
+        htmlTbody += '<td>' + data[i].Doc + '</td>'
+        htmlTbody += '<td>' + data[i].Codigo + '</td>'
+        htmlTbody += '<td>' + data[i].Originador + '</td>'
+        htmlTbody += '<td>' + data[i].NomDocumento + '</td>'
+        htmlTbody += '<td>' + data[i].FechaElaboracion + '</td>'
+        htmlTbody += '<td>' + data[i].RevisionActual + '</td>'
+        htmlTbody += '<td>' + data[i].Elaboracion + '</td>'
+        htmlTbody += '<td>' + data[i].RevisionAprobacion + '</td>'
+        htmlTbody += '<td>' + data[i].AprobadoInvolucrados + '</td>'
+        htmlTbody += '<td>' + data[i].Difusion + '</td>'
+        htmlTbody += '<td>' + data[i].FechaUltimaRevision + '</td>'
+        htmlTbody += '<td>' + data[i].Observaciones + '</td>'
+        htmlTbody += '<td>' + data[i].estatus + '</td>'
+        htmlTbody += '</tr>'
+
+    }
+
+    $("#tbodyPrincipal").append(htmlTbody)
+}
+
+function check() {
+    //if ($(this).attr('checked')) {
+    //    $(this).removeAttr('checked')
+    //    var a = $(this).attr('checked')
+    //    alert(a)
+    //} else {
+    //    $(this).attr('checked', 'checked')
+    //    var a = $(this).attr('checked')
+    //    alert(a)
+    //}
+}
+
 
 
 function prueba() {
@@ -70,31 +169,6 @@ function prueba() {
     htmlTbody += '<td>Habilitado</td>'
     htmlTbody += '</tr>'
 
-
-
-
     $("#tbodyPrincipal").append(htmlTbody)
-
 }
 
-function obtData() {
-    var data=[];
-    data[0].Nivel = "2"
-    data[0].Area = "LP"
-    data[0].Numero="01"
-    data[0].Codigo = "2-LP-01"
-    data[0].Originador = "Marisela Domínguez"
-    data[0].Nombre = "Procedimiento de Inspeccion Recibo Pelota"
-    data[0].FechaElaboracion = "09-11-18"
-    data[0].RevisionAct = "0"
-    data[0].Elaboracion = "ok"
-    data[0].RevisionAprobacion = "ok"
-    data[0].Aprobado = "ok"
-    data[0].Difusion = "ok"
-    data[0].FechaRevision = "09-11-18"
-    data[0].Observacion = ""
-    data[0].Estatus="Habilitado"
-
-    return data;
-
-}
